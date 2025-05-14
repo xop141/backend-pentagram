9098230a1206f69ba136b2ea91ab870b1e1d8779
xop141 <lowsens400@gmail.com>
2025-05-14 09:17:14 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://newwinstagram.vercel.app",'https://pentagram-i97c.onrender.com'],
+    origin: ["http://localhost:3000", "https://pentagram-i97c.onrender.com"],
   })
 );
 
